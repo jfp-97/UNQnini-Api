b9ef6b18c36505025a5a00a4de58cd936a501c89
Juan Francisco Perez <juanf.perez.97@gmail.com>
2022-09-13 11:42:25 -0300

    feature/UQN-21/incializar-repo-backend

---
+++ src/test/java/ar/unq/unqnini/UnqniniApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.unq.unqnini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnqniniApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
