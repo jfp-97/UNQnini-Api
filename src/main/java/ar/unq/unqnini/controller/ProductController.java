a3166a272027d4362bec0aff42b4404e1ed2f319
JPContardo <contardo.juanpablo@gmail.com>
2022-09-16 14:54:08 -0300

    Se agrego una verificacion para cuando no se encuentran productos.

--- src/main/java/ar/unq/unqnini/controller/ProductController.java
+++ src/main/java/ar/unq/unqnini/controller/ProductController.java
@@ -2,17 +2,30 @@ package ar.unq.unqnini.controller;
 import ar.unq.unqnini.model.Product;
 import ar.unq.unqnini.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 import java.util.List;
 
 @RestController
+@ControllerAdvice
 @RequiredArgsConstructor
 public class ProductController {
     private final ProductService productService;
 
     @GetMapping("/products")
     public List<Product> getProducts() {
-        return productService.getAllProducts();
+        List<Product> products = productService.getAllProducts();
+
+        if(products.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "There are no products available");
+        } else {
+            return products;
+        }
     }
+
+
+
 }
